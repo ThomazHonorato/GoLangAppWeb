ef99c9a3c978ce327ac363a4aff7dd18683876c1
Thomaz Honorato <thomaz.honorato@gmail.com>
2024-10-27 00:20:14 -0300

    Criado servidor e primeiro template.

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+)
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	produtos := []Produto{
+		{
+			Nome:       "Camiseta",
+			Descricao:  "Bem Bonita",
+			Preco:      39,
+			Quantidade: 5,
+		},
+		{
+			"Tenis",
+			"Confortavel",
+			89,
+			3,
+		},
+		{
+			"Fone",
+			"Muito Bom",
+			59,
+			2,
+		},
+	}
+	templates.ExecuteTemplate(w, "Index", produtos)
 }
