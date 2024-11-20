325910b287cc3232634a9930af48b30727006198
Thomaz Honorato <thomaz.honorato@gmail.com>
2024-11-20 13:51:09 -0300

    Adicionando metodo de inserção no banco de dados

--- controllers/produtosController.go
+++ controllers/produtosController.go
@@ -3,13 +3,42 @@ package controllers
 import (
 	"GoLangAppWeb/models"
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 )
 
-var temp = template.Must(template.ParseFiles("templates/index.html"))
+var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	todosOsProdutos := models.BuscaTodosOsProdutos()
 
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço:", err)
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão do preco:", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoConvertido, quantidadeConvertida)
+	}
+	http.Redirect(w, r, "/", 301)
 }
